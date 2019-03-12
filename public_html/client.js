d6b823ee62bfc5c3cb993c2a01927610c81acb33
kolson44 <kolson352@mcad.edu>
2019-03-12 14:31:07 -0500

    first network working version

---
+++ public_html/client.js
@@ -0,0 +1,146 @@
+let socket = io.connect();
+
+//can reference this for the team
+let team;
+let bluePoints = 0;
+let pinkPoints = 0;
+
+
+//choose a team
+let teamGen = Math.floor(Math.random() * 3)
+console.log(teamGen)
+
+if (teamGen == 0) {
+  team = 'pink'
+} else if (teamGen == 1) {
+  team = 'blue'
+} else if (teamGen == 2) {
+  team = 'white'
+}
+
+console.log(team);
+
+
+//setting the background color based on team choice
+if (team === 'blue') {
+  $('.team1').text('TEAM BLUE')
+  $('body').css('background-color', 0, 191, 255)
+
+
+} else if (team === 'pink') {
+  $('.team1').text('TEAM PINK')
+  $('body').css('background-color', 'pink')
+
+
+} else if (team === 'white') {
+  $('.team1').text('TEAM WHITE')
+  $('body').css('background-color', 'white')
+  $('.team1').css('color', 'black')
+  $('.timer').css('color', 'black')
+
+
+}
+
+
+
+socket.on('count', function(timeleft) {
+  console.log('time:', timeleft)
+
+  if (timeleft > 0) {
+    $("#countdown").text(timeleft + " sec ");
+  }
+
+  //the game is over
+  if (timeleft == 0) {
+    //check each grid cell for its color
+    //we can do this once to check the status and get the score!
+    $('.grid').children().each(function(i, child) {
+      //console.log(child)
+
+      // console.log($(child).hasClass('red'))
+
+      //if its color is the one were looking for then add a point
+      if ($(child).hasClass('blue')) {
+
+        bluePoints = bluePoints + 1;
+
+      } else if ($(child).hasClass('pink')) {
+
+        pinkPoints = pinkPoints + 1;
+      }
+
+    }) //close the each child loop
+
+    // console.log(bluePoints, pinkPoints)
+
+    $('.results').show();
+    $('.bluepoint').text(bluePoints)
+    $('.pinkpoint').text(pinkPoints)
+  } //close the 30 second check.
+
+  if(timeleft < 0){
+    $("#countdown").text('Game is Finished! Waiting for the next round!')
+  }
+
+  if (timeleft == -15) {
+    location.reload()
+  } //close the 45 second check
+
+}) //close the server's incoming universal timer
+
+
+//change the cell color based on the keypress (based ont the team choice)
+$(window).on('keypress', function(eventData) {
+  //if this is the spacebar...
+  if (eventData.charCode == 32) {
+
+    let randomNum = Math.round(Math.random() * 205 + 1)
+
+    if (team === 'blue') {
+
+      socket.emit('colorchange', {
+        'team': 'blue',
+        'cellToChange': '.item' + randomNum
+      })
+
+      // $('.item'+randomNum).css('background-color', 'blue').addClass('blue')
+
+
+    } else if (team === 'pink') {
+      socket.emit('colorchange', {
+        'team': 'pink',
+        'cellToChange': '.item' + randomNum
+      })
+
+      // $('.item'+randomNum).css('background-color', 'pink').addClass('pink')
+
+    } else if (team === 'white') {
+      socket.emit('colorchange', {
+        'team': 'white',
+        'cellToChange': '.item' + randomNum
+      })
+
+      // $('.item'+randomNum).css('background-color', 'white').addClass('white')
+
+    }
+
+  }
+})
+
+
+
+//Listen for any changes from the server
+
+socket.on('resultColorChange', function(incomingColorData) {
+
+  if (incomingColorData.team == 'blue') {
+    $(incomingColorData.cellToChange).css('background-color', 'blue').addClass('blue')
+  } else if (incomingColorData.team == 'pink') {
+    $(incomingColorData.cellToChange).css('background-color', 'pink').addClass('pink')
+  } else if (incomingColorData.team == 'white') {
+    $(incomingColorData.cellToChange).css('background-color', 'white').addClass('white')
+
+  }
+
+
+})
--- server.js
+++ server.js
@@ -1,14 +1,60 @@
 //make express available
 const express = require('express');
-//make socket io available
-const io = require('socket.io');
+
 //turn on express
 const app = express();
 // Make a server to handle TCP connection...
 const server = require('http').Server( app );
 
+//make socket io available
+const io = require('socket.io')(server);
+
 //server out files in our public_html folder
 app.use(express.static('public_html'));
+
+
+
+io.on('connect', function(socket){
+
+
+  //log out the unique
+  console.log(socket.id);
+
+  socket.on('colorchange', function(incomingColorData){
+    console.log(incomingColorData)
+
+    io.emit('resultColorChange', incomingColorData)
+
+  })
+
+})
+
+
+
+//game timer functionality
+var timeleft = 30; //total game play time including menus
+
+//this timer loops forever!
+var downloadTimer = setInterval(function(){
+
+  timeleft -= 1; //count down the timer
+
+  //send universal timer down to everyone
+  io.emit('count', timeleft)
+
+  //-15 is the buffer for the points notification
+  if(timeleft <= -15){
+    //timer finsihes
+    timeleft = 30
+
+  }// close the loop that looks at each grid child
+
+
+
+}, 1000); //every second
+
+
+
 
 //turn on our server so it can recieve request.
 server.listen(3000, function(){
